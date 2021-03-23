aa1de1f28d650f5fb7bd322f51344416bfa399f0
BraianStrak <braianstrak1999@gmail.com>
2021-03-23 23:11:03 +0000

    Created components

---
+++ personal-website-frontend/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import About from "./About"
+import Skills from "./Skills"
+import Contact from "./Contact"
+
+
+
+function Navbar() {
+  return (
+    <div>
+      returns links to three pages
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ personal-website-frontend/src/components/Skills.js
@@ -0,0 +1,10 @@
+function Skills() {
+    return (
+      <div>
+        Skills page
+      </div>
+    );
+  }
+  
+  export default Skills;
+  
