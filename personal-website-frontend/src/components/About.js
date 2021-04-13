483261a5fccf5fd72b4b81ac4e3a78f20998616c
BraianStrak <braianstrak1999@gmail.com>
2021-04-13 23:11:15 +0100

    Begun adding animations

--- personal-website-frontend/src/components/About.js
+++ personal-website-frontend/src/components/About.js
@@ -1,12 +1,18 @@
+import FadeIn from 'react-fade-in';
+
 function About() {
     return (
       <div>
+        <FadeIn delay = {0} transitionDuration = {600}>
         <h1>
             Hey, I'm Braian.
         </h1>
+        </FadeIn>
+        <FadeIn delay = {1500} transitionDuration = {600}>
         <h2>
             A full stack developer and Computer Science student based in London, England.
         </h2>
+        </FadeIn>
       </div>
     );
   }
--- personal-website-frontend/src/components/Skills.js
+++ personal-website-frontend/src/components/Skills.js
@@ -1,13 +1,57 @@
 import express from  "../assets/express.png"
+import android from  "../assets/android.png"
+import cplusplus from  "../assets/cplusplus.png"
+import java from  "../assets/java.png"
+import mongodb from  "../assets/mongodb.png"
+import nodejs from  "../assets/nodejs.png"
+import rails from  "../assets/rails.png"
+import react from  "../assets/react.png"
+
+import FadeIn from 'react-fade-in';
+
 
 function Skills() {
     return (
-      <div className = "">
-        <div className="skill-wrapper d-flex flex-column align-items-center justify-content-center">
-          <img className="skill-icon" src={express} alt="express icon"/>
-          <h4>Express JS</h4>
+      <div className = "col">
+        <div className="row">
+          <h1>I'm experienced in:</h1>
         </div>
+
         
+        <FadeIn className="col d-flex flex-wrap">
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={express} alt="express icon"/>
+            <h4>Express JS</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={mongodb} alt="express icon"/>
+            <h4>MongoDB</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={nodejs} alt="express icon"/>
+            <h4>Node JS</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={react} alt="express icon"/>
+            <h4>React</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={rails} alt="express icon"/>
+            <h4>Ruby on Rails</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={android} alt="express icon"/>
+            <h4>Android</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={java} alt="express icon"/>
+            <h4>Java</h4>
+          </div>
+          <div className="col skill-wrapper d-flex flex-column align-items-center justify-content-center">
+            <img className="skill-icon" src={cplusplus} alt="express icon"/>
+            <h4>C++</h4>
+          </div>
+        </FadeIn>
       </div>
     );
   }
