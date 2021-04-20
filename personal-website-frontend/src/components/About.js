a7286417c5aca6e5f11e18cdd2124556f5300cb5
BraianStrak <braianstrak1999@gmail.com>
2021-04-20 09:18:34 +0100

    Added personal picture

--- personal-website-frontend/src/components/About.js
+++ personal-website-frontend/src/components/About.js
@@ -1,13 +1,21 @@
 import FadeIn from 'react-fade-in';
+import portrait from '../assets/personalpicture.png';
 
+// I want this to return a Div either on top, or on the left, with my profile picture. 
+// perhaps smaller screens will have the picture on top and bigger screens will have it on the side? 
 function About() {
     return (
-      <div>
+      <div className = "d-flex flex-column justify-content-center align-items-center">
+        <FadeIn delay = {0} transitionDuration = {600}>
+          <img className="portrait pb-2" src={portrait} alt="portrait"/>
+        </FadeIn>
+
         <FadeIn delay = {0} transitionDuration = {600}>
         <h1>
             Hey, I'm Braian.
         </h1>
         </FadeIn>
+
         <FadeIn delay = {800} transitionDuration = {600}>
         <h2>
             A full stack developer and Computer Science student based in London, England.
